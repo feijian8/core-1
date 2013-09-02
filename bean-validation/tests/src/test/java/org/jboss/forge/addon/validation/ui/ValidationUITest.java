ddc41193f994ba4eb443bf7c18e3a2a9d3428caf
George Gastaldi <gegastaldi@gmail.com>
2013-09-02 12:24:22 -0300

    FORGE-885: Implemented bean-validation addon

---
+++ bean-validation/tests/src/test/java/org/jboss/forge/addon/validation/ui/ValidationUITest.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright 2013 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.jboss.forge.addon.validation.ui;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.forge.addon.validation.ManagedExportedObject;
+import org.jboss.forge.arquillian.AddonDependency;
+import org.jboss.forge.arquillian.Dependencies;
+import org.jboss.forge.arquillian.archive.ForgeArchive;
+import org.jboss.forge.furnace.repositories.AddonDependencyEntry;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.junit.runner.RunWith;
+
+/**
+ * 
+ * @author <a href="ggastald@redhat.com">George Gastaldi</a>
+ */
+@RunWith(Arquillian.class)
+public class ValidationUITest
+{
+   @Deployment
+   @Dependencies({ @AddonDependency(name = "org.jboss.forge.addon:bean-validation"),
+            @AddonDependency(name = "org.jboss.forge.addon:ui"),
+            @AddonDependency(name = "org.jboss.forge.furnace.container:cdi") })
+   public static ForgeArchive getDeployment()
+   {
+      ForgeArchive archive = ShrinkWrap
+               .create(ForgeArchive.class)
+               .addClass(ManagedExportedObject.class)
+               .addBeansXML()
+               .addAsAddonDependencies(
+                        AddonDependencyEntry.create("org.jboss.forge.addon:ui"),
+                        AddonDependencyEntry.create("org.jboss.forge.addon:bean-validation"),
+                        AddonDependencyEntry.create("org.jboss.forge.furnace.container:cdi"));
+
+      return archive;
+   }
+
+}
