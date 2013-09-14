37581328f80d66b43ade7eace00fe48f15aca4ca
George Gastaldi <gegastaldi@gmail.com>
2013-09-14 00:06:00 -0300

    FORGE-885: Created validator for InputComponent. Tests still fail

--- bean-validation/tests/src/test/java/org/jboss/forge/addon/validation/ui/ValidationUITest.java
+++ bean-validation/tests/src/test/java/org/jboss/forge/addon/validation/ui/ValidationUITest.java
@@ -7,14 +7,27 @@
 
 package org.jboss.forge.addon.validation.ui;
 
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.validation.constraints.Size;
+
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.forge.addon.validation.ManagedExportedObject;
+import org.jboss.forge.addon.ui.context.AbstractUIContext;
+import org.jboss.forge.addon.ui.context.AbstractUIValidationContext;
+import org.jboss.forge.addon.ui.context.UIContext;
+import org.jboss.forge.addon.ui.context.UISelection;
+import org.jboss.forge.addon.ui.input.UIInput;
+import org.jboss.forge.addon.ui.metadata.WithAttributes;
+import org.jboss.forge.addon.ui.util.Selections;
 import org.jboss.forge.arquillian.AddonDependency;
 import org.jboss.forge.arquillian.Dependencies;
 import org.jboss.forge.arquillian.archive.ForgeArchive;
 import org.jboss.forge.furnace.repositories.AddonDependencyEntry;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
@@ -32,7 +45,6 @@ public class ValidationUITest
    {
       ForgeArchive archive = ShrinkWrap
                .create(ForgeArchive.class)
-               .addClass(ManagedExportedObject.class)
                .addBeansXML()
                .addAsAddonDependencies(
                         AddonDependencyEntry.create("org.jboss.forge.addon:ui"),
@@ -42,4 +54,33 @@ public class ValidationUITest
       return archive;
    }
 
+   @Inject
+   @WithAttributes(label = "Name", required = true)
+   @Size(min = 1, max = 5)
+   private UIInput<String> name;
+
+   @Test
+   public void testValidation()
+   {
+      name.setValue("A very long string");
+      final UIContext uiContext = new AbstractUIContext()
+      {
+         @Override
+         public <SELECTIONTYPE> UISelection<SELECTIONTYPE> getInitialSelection()
+         {
+            return Selections.emptySelection();
+         }
+      };
+      AbstractUIValidationContext validationContext = new AbstractUIValidationContext()
+      {
+         @Override
+         public UIContext getUIContext()
+         {
+            return uiContext;
+         }
+      };
+      name.validate(validationContext);
+      List<String> errors = validationContext.getErrors();
+      Assert.assertFalse("An error should have been captured", errors.isEmpty());
+   }
 }
